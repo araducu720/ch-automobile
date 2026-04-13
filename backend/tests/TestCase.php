5529c41ea7ccd082f575558fa059951241a72ea8
araducu720 <araducu720@gmail.com>
2026-04-13 19:44:39 +0000

    fix: disable throttle middleware in tests to prevent 429s

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware(ThrottleRequests::class);
+    }
 }
